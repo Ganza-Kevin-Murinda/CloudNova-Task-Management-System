a17f7448db1235346235c1f52a2d40d37b5ea142
kevin <murindakevinmulk@gmail.com>
2025-05-31 23:03:44 +0200

    Modify Email Exception

--- src/main/java/com/taskmgt/CloudNova/exception/DuplicateEmailException.java
+++ src/main/java/com/taskmgt/CloudNova/exception/DuplicateEmailException.java
@@ -1,7 +1,15 @@
 package com.taskmgt.CloudNova.exception;
 
+/**
+ * Exception thrown when attempting to create or update a user with an email that already exists
+ */
 public class DuplicateEmailException extends RuntimeException {
+
   public DuplicateEmailException(String email) {
     super("User with email '" + email + "' already exists");
+  }
+
+  public DuplicateEmailException(String message, Throwable cause) {
+    super(message, cause);
   }
 }
